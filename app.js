4496b2ff67e3b3983b564f6db3c2dc8c59bc1f10
PabloSantaMaria <pabloj.santamaria@gmail.com>
2020-10-26 00:09:17 -0300

    ref model

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// URI=mongodb://127.0.0.1:27017/Nefli
-// URI=mongodb+srv://admin:admin@cluster0.rdo9y.mongodb.net/Netflix?retryWrites=true&w=majority
 const bodyParser = require('body-parser');
 require('dotenv/config');
 
@@ -13,7 +11,7 @@ const app = express();
 const PORT = 3000;
 
 // Connect to DB
-const db = mongoose.connect(
+mongoose.connect(
     process.env.URI, 
     {useNewUrlParser: true, useUnifiedTopology: true}, 
     () => console.log('mongoDB connected')
@@ -26,5 +24,3 @@ app.use('/movies', moviesRouter);
 
 //Start listening
 app.listen(PORT);
-
-module.exports = db;
